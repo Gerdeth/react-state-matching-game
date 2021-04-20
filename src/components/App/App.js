520091571b9cdd90e7e83ce9a943ef951c79aaec
Gerdeth <gerdethwickham@gmail.com>
2021-04-20 07:06:13 -0400

    pass start game method to button

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -33,7 +33,7 @@ class App extends Component{
       <header className="App-header">
         Turbo-Matcher
       </header>
-        <OptionsPanel playing={this.state.playing} numTiles={this.state.numTiles} />
+        <OptionsPanel playing={this.state.playing} numTiles={this.state.numTiles} startGame={this.startGame} />
         <Board tiles={this.state.tiles} numTiles={this.state.numTiles} />
       
     </div>
