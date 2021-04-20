f0d536b2bc75dcf1524155db58737be211ed7a21
Gerdeth <gerdethwickham@gmail.com>
2021-04-20 06:50:38 -0400

    conditionally render the svg

--- src/components/Tile/Tile.js
+++ src/components/Tile/Tile.js
@@ -6,6 +6,7 @@ const Tile = (props) => {
   const dynamicColor=props.selected || props.matched ? {backgroundColor: props.color}:null
   return (
     <div className='Tile'style ={dynamicColor}>
+      props.selected || props.matched ? <props.svg/>:null
     </div>
   )
 }
