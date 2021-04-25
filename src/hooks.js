18ec462e1070cf6e736e35f58d72014d6c3737b1
Gerdeth <gerdethwickham@gmail.com>
2021-04-25 08:09:48 -0400

    create callback on mouse events

--- src/hooks.js
+++ src/hooks.js
@@ -6,10 +6,17 @@ function useHover(){
  const ref =useRef()
  const [hovered, setHovered]= useState(false)
 
+ function enter(){
+     setHovered(true)
+ }
+
+ function leave() {
+     setHovered(false)
+ }
  useEffect(()=>{
      const refCopy= ref
-     refCopy.current.addEventListener('mouseenter',()=>{})
-     refCopy.current.addEventListener('mouseleave',()=>{})
+     refCopy.current.addEventListener('mouseenter', enter())
+     refCopy.current.addEventListener('mouseleave',leave())
      return ()=>{
 
      }
